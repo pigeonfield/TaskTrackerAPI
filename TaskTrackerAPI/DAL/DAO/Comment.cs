65ef394e2a31e1f30f14fa073c80d6fb253de823
Aleksandra Kiszka <aleksandra.kiszka91@gmail.cm>
2018-12-11 11:05:42 +0100

    Reorganize project structure

---
+++ TaskTrackerAPI/DAL/DAO/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTrackerAPI.DAL.DAO
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+
+        public string Message { get; set; }
+
+        public Task Task { get; set; }
+        public int TaskId { get; set; }
+
+    }
+}
---
+++ TaskTrackerAPI/DAL/DAO/Task.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskTrackerAPI.DAL.DAO;
+
+namespace TaskTrackerAPI.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string LongDescription { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Priority { get; set; }
+
+        public bool IsDone { get; set; }
+
+
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+        public List<Comment> Comments { get; set; }
+        = new List<Comment>();
+
+
+    }
+}
